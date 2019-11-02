c680c5e1b6251b47c78ae45244c098727ec49fb5
hoa phat <hoaphat.nguyenphan@gmail.com>
2019-11-02 21:18:11 +0700

    add utils folder, including utils: api, route

---
+++ src/utils/routes.ts
@@ -0,0 +1,36 @@
+type Page = 'home' | 'about' | 'contact' | 'tag' | 'post';
+type Route = {
+	path: string;
+	to: string;
+	activeClassName: string;
+};
+
+const routes: Record<Page, Route> = {
+	home: {
+		path: '/',
+		to: '/',
+		activeClassName: 'actived'
+	},
+	about: {
+		path: '/about',
+		to: '/about',
+		activeClassName: 'actived'
+	},
+	contact: {
+		path: '/contact',
+		to: '/contact',
+		activeClassName: 'actived'
+	},
+	tag: {
+		path: '/:tag',
+		to: '/example-tag',
+		activeClassName: 'actived'
+	},
+	post: {
+		path: '/:category/:post',
+		to: '/example-category/example-post',
+		activeClassName: 'actived'
+	}
+};
+
+export default routes;
